62078c4bce63b319c4cf3e39658409d901d71cba
K0T1KK <rich050606@gmail.com>
2024-09-18 02:42:48 +0900

    Create Main.kt

---
+++ src/Main.kt
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    print("Введите число:")
+
+    val a = readLine()
+    //1.4
+    println()
+    print(" Вы ввели число $a")
+
+    //1.5
+    println()
+    println()
+    println("$a - вот такое число вы ввели")
+}
+
